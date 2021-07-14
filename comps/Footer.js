c8816fd9332f35941872aab3c916525468f6f95a
hiep <vanhiep2102@gmail.com>
2021-07-14 16:37:11 +0700

    first commit

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            Copy right by Ninja
+        </footer>
+     );
+}
+ 
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return ( 
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo ea nulla impedit. Delectus est in debitis sed nesciunt totam ducimus, natus autem repudiandae, tenetur unde. Doloremque, quam minima esse perspiciatis excepturi at fuga odio, reprehenderit dolorum voluptates repudiandae quidem? Voluptatibus harum dolor et quos voluptate impedit atque accusamus. Necessitatibus, magnam quod quas officiis et, iure blanditiis ipsam fuga itaque ducimus ullam nulla, harum saepe modi rerum ipsum voluptate iusto. Maiores vero error, ea optio laudantium mollitia quam beatae accusamus earum.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo ea nulla impedit. Delectus est in debitis sed nesciunt totam ducimus, natus autem repudiandae, tenetur unde. Doloremque, quam minima esse perspiciatis excepturi at fuga odio, reprehenderit dolorum voluptates repudiandae quidem? Voluptatibus harum dolor et quos voluptate impedit atque accusamus. Necessitatibus, magnam quod quas officiis et, iure blanditiis ipsam fuga itaque ducimus ullam nulla, harum saepe modi rerum ipsum voluptate iusto. Maiores vero error, ea optio laudantium mollitia quam beatae accusamus earum.</p>
+            </div>
+        </>
+        
+     );
+}
+ 
+export default About;
